16ceece40054584306536c8d5fda147ef956a851
amirreza <amir.rsf1380@gmail.com>
2023-08-05 02:55:24 +0330

    initial commit

---
+++ books/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+import uuid
+
+
+class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
